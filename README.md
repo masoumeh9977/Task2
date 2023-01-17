aaa786c4edec6493230962318e53a9e9e8c741a3
masoumeh9977 <masoumeh99_work@yahoo.com>
2023-01-18 03:05:43 +0330

    implementation of second query :)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This project consists of 2 SQL query.
 You need to insert some records to the tables at first.
+Both queries are in the web.php file.
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Topic;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,10 @@ Route::get('/', function () {
 
     //First Query of the Task
     $topic = Topic::withCount('tickets')->findOrFail(1);
-    dd($topic->tickets_count);
+    //dd($topic->tickets_count);
 
+    //Second Query of the Task
+    $tickets_count = $topic->tickets_count;
+    $topics_count = Topic::where('id', '<>', $topic->id)->where('topic_parent_id', $topic->id)->get()->count();
+    dd($tickets_count + $topics_count);
 });
